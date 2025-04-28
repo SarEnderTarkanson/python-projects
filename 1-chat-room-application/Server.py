3c823023bfc5ad49ce310e1c5491dde00421dd55
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-28 02:17:58 +0200

    created a thread to accept multiple clients to connect

--- 1-chat-room-application/Server.py
+++ 1-chat-room-application/Server.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 host = "localhost"
 port = 8080
@@ -9,6 +10,18 @@ addresses = {}
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port))
 
-if __name__=="__main__":
+
+def accept_clients_connections():
+    while True:
+        client_conn, client_address = sock.accept()
+        print(client_address, "Has connected")
+        client_conn.send("Welcome to the chatroom. Please type your name to continue.".encode("utf8"))
+        addresses[client_conn] = client_address
+
+if __name__ == "__main__":
     sock.listen(5)
     print("the server is running and is listening to clients' requests.")
+
+    t1 = Thread(target=accept_clients_connections)
+    t1.start()
+    t1.join()
